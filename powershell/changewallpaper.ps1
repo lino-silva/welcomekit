2bcb2f23af6eaf8f1e0765c37aa272dfec73d878
tacf <dev.tiago@gmail.com>
2016-09-21 08:31:03 +0100

    Change Wallpaper
    José Cid as example

---
+++ powershell/changewallpaper.ps1
@@ -0,0 +1,10 @@
+﻿Function Set-WallPaper($Value)
+{
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
+
+Invoke-WebRequest "https://novotomo.files.wordpress.com/2015/06/jcid.jpg" -OutFile C:\temp\test.jpg
+
+Set-WallPaper "C:\temp\test.jpg"
+
