a36b9709d30f97695fa26ea26f4d5a4e95b0afc3
tacf <dev.tiago@gmail.com>
2016-09-21 14:06:58 +0100

    Changed the way the change wallpaper is executed
    Change Wallpaper should now work on windows

--- powershell/changewallpaper.ps1
+++ powershell/changewallpaper.ps1
@@ -1,10 +1,52 @@
-﻿Function Set-WallPaper($Value)
-{
- Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
- rundll32.exe user32.dll, UpdatePerUserSystemParameters
-}
-
+﻿
+Write-Host "Downloading picture"
 Invoke-WebRequest "https://novotomo.files.wordpress.com/2015/06/jcid.jpg" -OutFile C:\temp\test.jpg
 
-Set-WallPaper "C:\temp\test.jpg"
+Write-Host "Setting Wallpaper"
 
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+namespace Wallpaper
+{
+   public enum Style : int
+   {
+       Tile, Center, Stretch, NoChange
+   }
+   public class Setter {
+      public const int SetDesktopWallpaper = 20;
+      public const int UpdateIniFile = 0x01;
+      public const int SendWinIniChange = 0x02;
+      [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+      private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+      public static void SetWallpaper ( string path, Wallpaper.Style style ) {
+         SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+         RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+         switch( style )
+         {
+            case Style.Stretch :
+               key.SetValue(@"WallpaperStyle", "2") ; 
+               key.SetValue(@"TileWallpaper", "0") ;
+               break;
+            case Style.Center :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "0") ; 
+               break;
+            case Style.Tile :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "1") ;
+               break;
+            case Style.NoChange :
+               break;
+         }
+         key.Close();
+      }
+   }
+}
+"@
+
+[Wallpaper.Setter]::SetWallpaper( 'C:\temp\test.jpg', 0 )
+
+Write-Host "Removing Temp file"
+Remove-Item "C:\temp\test.jpg"
